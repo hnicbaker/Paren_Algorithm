fa028ba2ca8a9677f6aeb22d4f3dd003820c1e12
Heather Baker <heather@heatherdev.net>
2019-05-22 09:44:01 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+function validateCode(string){
+
+    let counter = 0;
+    //initialize counter that will keep track of open parens without closed parens
+    let chars = string.split('');
+    // Split string at spaces & create array of words/chars
+  
+    for(let i = 0; i < chars.length; i++) {
+    //iterate through array 
+      if(chars[i] == '('){
+        counter++
+    // if open paren is found, increment counter
+  
+      } else if (chars[i] == ')'){
+        counter--  
+    //if closed paren is found, decrement counter
+        if (counter < 0){
+          return false; 
+    //first check - if counter is less than zero, return false, because parens are out of order 
+        }
+      }
+  
+    }
+  
+    if (counter == 0 ){
+      return true;
+    } else return false;
+    //second check - if counter is equal to 0 at end of iteration, parens are equal 
+  
+  }
+  
+  // validateCode("if ((x == y/z) && (q - t == 0))(")
+  validateCode("if ((x == y/z) && (q - t == 0))")
+  
+  
+  
